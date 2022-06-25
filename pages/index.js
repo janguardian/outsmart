067c2dbcac3f0270831d05a748049a103e7e8203
Anton Kharchenko <norchah@yandex.ru>
2022-06-25 17:55:22 +0200

    fix: Fixed many bugs + new feats after post in the Slack

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,15 @@
 import './index.css'
+
+// const cardSizeS = document.querySelectorAll('.card_size_m');
+
+//     cardSizeS.forEach(function(card) {
+//         card.addEventListener('mouseenter', (e) => {
+//             card.classList.add('card_theme_dark',
+//         'card_background_blue-purple');
+//         })
+//         card.addEventListener('mouseleave', () => {
+//             card.classList.remove('card_theme_dark',
+//         'card_background_blue-purple');
+//         })
+//     })
+
